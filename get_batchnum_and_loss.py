75b612d099571a0ef9a5765f997a47ae150bd8e5
NMB1331 <natemburl@gmail.com>
2017-07-24 15:48:01 -0700

    Stores batch number/loss rate to file

--- get_batchnum_and_loss.py
+++ get_batchnum_and_loss.py
@@ -2,28 +2,49 @@ import sys
 
 #Gets the file used for writing and the file to store the results in
 training_output_file = sys.argv[1]
-batch_loss_ouput_file = sys.argv[2]
+input_file_length = int(sys.argv[2]) - 1
+batch_loss_ouput_file = sys.argv[3]
 #For debugging
-print("Input file: %s\n", training_output_file)
-print("Output file: %s\n", batch_loss_ouput_file)
+print("Input file: {}".format(training_output_file))
+print("Input file length: {}".format(input_file_length))
+print("Output file: {}".format(batch_loss_ouput_file))
 
 #Opens the files for reading and writing
-infile = fopen(training_output_file, "r")
-outfile = fopen(batch_loss_ouput_file, "w")
+infile = open(training_output_file, "r")
+outfile = open(batch_loss_ouput_file, "w+")
 
-#Skips over the first 146 lines of the infile (Darknets setup crap)
+#Variables for reading data in declared
 counter = 0
 line = " "
-while (counter < 146):
-    line = infile.realine()
-#For debugging
-print("\nCurrent line: %s\n", line)
 
+#Skips over the first 146 lines of the infile (Darknets setup crap)
+while (counter < 147):
+    line = infile.readline()
+    #print(line)
+    counter += 1
 
+#Loops through the file one line at a time | 1818658
+while (counter < input_file_length):
+    line = infile.readline()
+    first_entry = line.split(' ', 1)[0]
+    second_item = line.split(', ')
+    #If the first entry in the line is numeric, (a batch number) writes line to file
+    if first_entry[0].isdigit():
+        #Batch number extracted from line
+        batch_number = first_entry[:-1]
+        print("Batch number: {}".format(batch_number))
 
+        #Loss rate extracted from line
+        loss_rate = (second_item[1])[:-4]
+        print("Loss rate: {}\n".format(loss_rate))
 
-
+        #Output information written to file
+        output_data = batch_number + ", " + loss_rate + "\n"
+        #print(output_data)
+        outfile.write(output_data)
+    #Increments counter
+    counter += 1
 
 #Closes the files
-fclose(infile)
-fclose(outfile)
+infile.close()
+outfile.close()
